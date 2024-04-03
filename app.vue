f353d7952152d94953df734e1319779afd2ecb0b
danielhill1982 <mail@dhill.dev>
2024-04-03 09:30:14 +0200

    bla

--- app.vue
+++ app.vue
@@ -4,21 +4,37 @@
 </script>
 
 <style>
-body, html {
+html {
   margin: 0;
-  background: url("https://picsum.photos/3840/2160?random=1");
-  
+  background: url("https://picsum.photos/3840/2160?random");
   height: 100%;
-
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
 }
 
+body {
+  margin: 0;
+}
+
+.textbg {
+  text-align: right;
+  width: 33.3%;
+  background-color: white;
+  margin-top: 6rem;
+  margin-left: 0;
+}
+
+.text {
+  padding: 2rem;
+  font-family: monospace, 'Courier New', Courier;
+  font-weight: bold;
+}
+
 </style>
 
 <template>
-    <div>
-      
+    <div class="textbg">
+      <p class="text">Here comes a new personal website, soon.</p>
     </div>
 </template> 
