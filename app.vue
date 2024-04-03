7419dc796200a5105a646b3ee6a2e42a40999062
danielhill1982 <mail@dhill.dev>
2024-04-03 19:00:36 +0200

    bla

--- app.vue
+++ app.vue
@@ -1,9 +1,46 @@
  
 <script setup lang="ts">
 
+import { gsap } from "gsap";
+
+onMounted(() => {
+window.addEventListener("load", () => {
+const heroOverlay = document.querySelector(".hero-overlay");
+const heroOverlayHeading = document.querySelector("#hero-overlay-heading");
+var tl = gsap.timeline();
+tl.from("#hero-overlay-line", {
+  duration: 5,
+  scaleX: 0,
+  transformOrigin: "right center",
+});
+tl.from(heroOverlayHeading, {
+  duration: 1.2,
+  opacity: 0,
+  y: 30,
+  transformOrigin: "bottom",
+  ease: "back",
+});
+tl.to(heroOverlay,{
+    duration: 1.5,
+    yPercent: 0,
+    height:0,
+    transformOrigin: "bottom"
+  });
+tl.to(heroOverlayHeading,{
+    duration: 1.5,
+    autoAlpha:0
+  },"<");
+tl.to("#hero-overlay-line", {
+    autoAlpha:0
+  },"<");
+});
+});
 </script>
 
 <style>
+*{
+  margin: 0;
+}
 html {
   margin: 0;
   background: url("https://picsum.photos/3840/2160?random");
@@ -17,24 +54,67 @@ body {
   margin: 0;
 }
 
-.textbg {
-  text-align: right;
-  width: 33.3%;
-  background-color: white;
-  margin-top: 6rem;
-  margin-left: 0;
+.base {
+  height:100vh;
+  
 }
 
-.text {
-  padding: 2rem;
-  font-family: monospace, 'Courier New', Courier;
-  font-weight: bold;
+.hero-overlay {
+  background: url("https://picsum.photos/3840/2160?random");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  color: #000;
+  position: absolute;
+  z-index:100;
+  inset:0;
 }
 
+.hero {
+  position:relative;
+  z-index:10;
+  color: #fff;
+  background-color: rgb(0, 0, 0, 0.75);
+}
+
+.heading-primary {
+  padding-top:40vh;
+  text-align:center;
+  font-size: 50px;
+}
+
+#hero-overlay-heading {
+  font-weight:700;
+}
+
+.line {
+  width: 300px;
+  height: 10px;
+  background: white;
+  margin-top:30px;
+  margin-inline: auto;
+
+}
+
+#hero-overlay-line {
+  background:#fff;
+}
 </style>
 
 <template>
-    <div class="textbg">
-      <p class="text">A new personal website is being created here.</p>
-    </div>
+    <div class="hero base">
+
+<h1 id="hero-heading" class="heading-primary">
+  A new personal website is <br>being created here.
+</h1>
+<div id="hero-line" class="line"></div>
+
+</div>
+<div class="hero-overlay base">
+
+<p id="hero-overlay-heading" class="heading-primary">
+  A new personal website is <br>being created here.
+</p>
+<div id="hero-overlay-line" class="line"></div>
+</div>
 </template> 
